a0a45dbc5b77fcf57106cd33ac829495af16f0ed
Siarhei Dudko <slavianich@gmail.com>
2023-01-15 19:18:24 +0400

    update repository structure add routines for mini_accountant dataset

--- README.md
+++ README.md
@@ -1,10 +1,36 @@
 # BigQuery Views
 
-## view for `firestore_export`
+## view for `firestore_export` (private)
 
-- `mini_accountant_invoices.sql`
+- `mini_accountant_invoices`
 
-## view for `mini_accountant`
+## view for `mini_accountant` (public)
 
-- `invoices.sql`
-- `users.sql`
+- `invoices`
+- `users`
+
+## routines for `mini_accountant`
+
+- `invoices_slice_by_range`
+- `positions_slice_by_range`
+- `transactions_slice_by_range`
+
+## example of create routine (table function)
+
+```sql
+CREATE OR REPLACE TABLE FUNCTION `invoicemaker-f5e1d.mini_accountant.transactions_slice_by_range`(
+    fromDate DATE, 
+    toDate DATE
+) AS (
+    -- the code of the table function
+)
+```
+
+## example of the call routine from Google Looker
+
+```sql
+SELECT * FROM `invoicemaker-f5e1d.mini_accountant.transactions_slice_by_range`(
+    PARSE_DATE("%Y%m%d",@DS_START_DATE), 
+    PARSE_DATE("%Y%m%d", @DS_END_DATE)
+);
+```
---
+++ mini_accountant/routines/invoices_slice_by_range.sql
@@ -0,0 +1,30 @@
+(
+    (
+        WITH `filtered_invoices` AS (
+            SELECT *
+            FROM `invoicemaker-f5e1d.mini_accountant.invoices` AS `invoice`
+            WHERE `invoice`.`DATE` >= CAST(fromDate AS STRING FORMAT "YYYYMMDD")
+                AND `invoice`.`DATE` <= CAST(toDate AS STRING FORMAT "YYYYMMDD")
+            ORDER BY `invoice`.`NUMBER`
+        )
+        SELECT `filtered_invoices`.`ID` AS `invoiceId`,
+            PARSE_DATE ("%Y%m%d", `filtered_invoices`.`DATE`) AS `invoiceDate`,
+            `filtered_invoices`.`NUMBER` AS `invoiceNumber`,
+            (
+                SELECT SUM(`position`.`PRICE`)
+                FROM UNNEST(`filtered_invoices`.`POSITIONS`) AS `position`
+            ) AS `invoiceAmount`,
+            `filtered_invoices`.`CUSTOMER` AS `customer`,
+            (
+                SELECT SUM(`transaction`.`SUM`)
+                FROM UNNEST(`filtered_invoices`.`TRANSACTIONS`) AS `transaction`
+            ) AS `paidAmount`,
+            (
+                SELECT SUM(
+                        `transaction`.`SUM` * `transaction`.`EXCHANGE_RATE`
+                    )
+                FROM UNNEST(`filtered_invoices`.`TRANSACTIONS`) AS `transaction`
+            ) AS `paidAmountInCurr`
+        FROM `filtered_invoices`
+    )
+)
